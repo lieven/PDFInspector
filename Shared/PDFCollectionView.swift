1c87dd74999af9b9ad6c8218118770d226e4765b
Lieven Dekeyser <lieven.dekeyser@showpad.com>
2020-07-05 14:37:34 +0200

    Display previews for most types

--- Shared/PDFCollectionView.swift
+++ Shared/PDFCollectionView.swift
@@ -27,27 +27,18 @@ struct PDFCollectionView: View {
     var body: some View {
         List(0..<values.count) { index in
             let (key, value) = values[index]
+            
             switch value {
             case .dictionary(let dict):
                 NavigationLink(destination: PDFCollectionView(title: key, dictionary: dict)) {
-                    PDFValueCell(key: key, value: "Dictionary", enabled: false)
+                    PDFValueCell(key: key, value: value.valuePreview)
                 }
-            case .string(let value), .name(let value):
-                PDFValueCell(key: key, value: value)
-            case .integer(let value):
-                PDFValueCell(key: key, value: "\(value)")
-            case .null:
-                PDFValueCell(key: key, value: "NULL", enabled: false)
-            case .boolean(let value):
-                PDFValueCell(key: key, value: value ? "true" : "false")
-            case .real(let value):
-                PDFValueCell(key: key, value: "\(value)")
             case .array(let array):
                 NavigationLink(destination: PDFCollectionView(title: key, array: array)) {
-                    PDFValueCell(key: key, value: "Array", enabled: false)
+                    PDFValueCell(key: key, value: value.valuePreview)
                 }
-            case .stream:
-                PDFValueCell(key: key, value: "Stream", enabled: false)
+            default:
+                PDFValueCell(key: key, value: value.valuePreview)
             }
         }
         .navigationTitle(title)
@@ -57,10 +48,12 @@ struct PDFCollectionView: View {
 
 struct PDFDictionaryView_Previews: PreviewProvider {
     static let testData: [(key: String, value: PDFObject)] = [
-        (key: "String Key", value: .string("Value1")),
-        (key: "Integer Key", value: .integer(1337)),
-        (key: "Null Key", value: .null),
-        (key: "Dictionary Key", value: .dictionary(nil))
+        (key: "String", value: .string("Value1")),
+        (key: "Integer", value: .integer(1337)),
+        (key: "Double 1", value: .real(1.0)),
+        (key: "Double 2", value: .real(1.337)),
+        (key: "Null", value: .null),
+        (key: "Dictionary", value: .dictionary(nil))
     ]
     
     static var previews: some View {
@@ -70,3 +63,56 @@ struct PDFDictionaryView_Previews: PreviewProvider {
     }
 }
 
+
+
+extension PDFObject {
+    var valuePreview: String {
+        switch self {
+        case .null:
+            return "NULL"
+        case .boolean(let value):
+            return value ? "true" : "false"
+        case .integer(let value):
+            return "\(value)"
+        case .real(let value):
+            return String(format: "%g", value)
+        case .name(let value), .string(let value):
+            return value
+        case .array(let array):
+            return array.arrayPreview
+        case .dictionary(let dictionary):
+            guard dictionary != nil else {
+                return "Invalid Dictionary"
+            }
+            return "Dictionary"
+        case .stream:
+            return "Stream"
+        }
+    }
+    
+    var isComplex: Bool {
+        switch self {
+        case .null, .boolean, .integer, .real, .name, .string:
+            return false
+        case .array, .dictionary, .stream:
+            return true
+        }
+    }
+}
+
+extension Optional where Wrapped == CGPDFArrayRef {
+    var arrayPreview: String {
+        switch self {
+        case .none:
+            return "Invalid Array"
+        case .some(let pdfArray):
+            let values = pdfArray.arrayValues
+            guard !values.contains(where: { $0.value.isComplex }) else {
+                return "Array"
+            }
+            
+            let valuePreviews = values.map { $0.value.valuePreview }
+            return "[\(valuePreviews.joined(separator: ", "))]"
+        }
+    }
+}
